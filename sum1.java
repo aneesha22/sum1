c1dc2c1bc2efe44b02531808764135657f808a8d
aneesha22 <42198319+aneesha22@users.noreply.github.com>
2018-08-09 22:55:28 +0530

    Create sum1.java

---
+++ sum1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int j=s.nextInt();
+		int n[]=new int[6];
+		int sum=0;
+		int k=s.nextInt();
+		for(int i=0;i<j;i++)
+		{
+			n[i]=s.nextInt();
+		}
+		for(int i=0;i<k;i++)
+		{
+			sum=sum=n[i];
+		}
+		System.out.println(sum);
+	}
+}
